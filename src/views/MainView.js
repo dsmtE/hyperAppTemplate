6911538cf3a4b3d4042c536f879d0752ea9a39cf
dsmtE <desmet.enguerrand@gmail.com>
2020-04-17 12:37:56 +0200

    UPDATE mainView

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -3,12 +3,15 @@ import { h } from 'hyperapp'
 import Button from '../components/Button'
 import BarChart from '../components/BarChart'
 
+import { parseEspaceVertsData } from '../services/sorting'
+import api from '../services/api'
+
 export default (state, actions) =>
     h('div', {}, [
         h('h1', {}, 'HyperApp Template'),
         h(
             'p',
-            { oncreate: () => actions.getIpFromApi() },
+            { oncreate: () => {actions.updateIp()} },
             'ton ip est :' + state.ip
         ),
         h('p', {}, 'count :' + state.count),
@@ -27,16 +30,22 @@ export default (state, actions) =>
             title: 'nombre d\'espaces verts par catégories',
             width: 800,
             height: 400,
-            callBack: (chart) => { // je défini ici une fonction de callback qui va être appellé après la création de mon diagramme
-                actions.getEspaceVertsDataFromApi({ // je fait un appel à l'action getEspaceVertsDataFromApi qui fait un appel à une base de donnée
-                    count: 200, // je lui passe en paramètre le nombre de ligne que je veux appeler via mon api
-                    callBack: (labels, data) => { // et une autre fonction de callBack qui sera appellé seulement après que mes données aient été reçus
-                        // ce qui me permet de mettre à jour l'affichage de mon diagramme qu'à ce moment là, une fois les données reçus
-                        chart.data.labels = labels
-                        chart.data.datasets[0].data = data
-                        chart.update({duration: 800})
-                    }
-                })
+            callBack: async (chart) => { // je défini ici une fonction de callback asynchrone qui va être appellé après la création de mon diagramme
+                const list = await api.getEspaceVertsData(200) // La fonction étant asynchrone me permet d'attendre un événement d'une autre fonction asynchrone avec le mot clé await
+                // Cela permet ici d'attendre que les données aient bien été reçu avant de faire la suite
+                const sorted = parseEspaceVertsData(list) // je trie mes données en fonctions de la forme de l'api (dépendra de chaque api et données)
+
+                // Ensuite je mets à jour mon chart avec les données (je suis obligé de faire cela avec chartJs car
+                // chartJs fait une copie de donnés, elles ne peuvent donc pas être liées directement à mon state, je suis obligé de lui repréciser les donnés)
+                // !!! => Si on veut pouvoir changer les donnés de ce diagramme plus tard il faut sauvegarder l'objet chart
+                // passé ici en paramètres dans notre state via une action pour pouvoir y avoir accès plus tard car il existe seulement ici que dans notre fonction de callBack
+                chart.data.labels = sorted.categories
+                chart.data.datasets[0].data = sorted.categoriesCount
+                chart.update({duration: 800})
+
+                // Ce n'est pas obligatoire car ici je ne me ressers pas de ses données mais je peux également déclencher une action
+                // ensuite afin de modifier le state et sauvegarder mes données dans celui-ci
+                actions.saveEspaceVertsData(sorted)
             }
         })
     ])
